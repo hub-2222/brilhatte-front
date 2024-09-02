0307969866067a54d7d4e69d896a0f58933d23e2
bruno-venturini <brunofreloventurini@outlook.com>
2024-09-01 21:06:48 -0300

    Ajustando tamanhos

--- app/card.tsx
+++ app/card.tsx
@@ -1,24 +1,31 @@
 import Image from "next/image";
 
-export default function Cards() {
+export default function Cards(props) {
     return (
         <div>
             <ul className="grid grid-cols-1 gap-y-10 gap-x-6 items-start p-8">
                 <li className="relative flex flex-col sm:flex-row items-start">
-                    <div className="order-1 sm:ml-6 ">
+                    <div className="order-1 sm:ml-4 ">
                         <h3 className="mb-1 text-slate-900 font-semibold">
-                            <span className="mb-1 block text-sm leading-6 text-indigo-500">Headless UI</span>Completely unstyled, fully accessible UI components
+                            <span className="mb-1 block text-sm leading-6 text-indigo-500">{props.nome}</span>
                         </h3>
                         <div className="prose prose-slate prose-sm text-slate-600">
-                            <p>Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind CSS.</p>
+                            <span>
+                                Pedras:
+                                <ul>
+                                    <li>
+
+                                    </li>
+                                </ul>
+                            </span>
                         </div>
                     </div>
                     <Image 
                         src="/img/ARTE_-_ELEG0018.jpg" 
                         alt=""
-                        className="mb-6 shadow-md w-full rounded-lg bg-slate-50 sm:w-[17rem] sm:mb-0"
+                        className="mb-6 h-26 w-24 shadow-md rounded-lg bg-slate-50 sm:mb-0"
                         width="1162" 
-                        height="1600" 
+                        height="1600"
                     />
                 </li>
             </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,31 @@ import Image from "next/image";
 import styles from '@/app/home.module.css';
 import clsx from 'clsx';
 import Cards from "./card";
+import {useState} from "react";
 
 
 export default function Home() {
+    const [fakeCard, setFakeCard] = useState({
+        nome: "ELEG0018 - 053156",
+        pedras: [
+            {
+                nome: "Sextavado black",
+                tamanho: "3mm",
+                quantidade: 24700
+            },
+            {
+                nome: "Sextavado gold",
+                tamanho: "3mm",
+                quantidade: 4439
+            },
+            {
+                nome: "Sextavado brown",
+                tamanho: "3mm",
+                quantidade: 10547
+            }
+        ]
+    });
+
   return (
         <main>
             <div className="flex items-center justify-center p-10 bg-primary">
@@ -18,7 +40,7 @@ export default function Home() {
             </div>
             
             <div>
-                <Cards/>
+                <Cards arte={fakeCard}/>
             </div>
         </main>
     );
