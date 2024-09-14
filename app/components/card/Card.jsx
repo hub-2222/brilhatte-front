dfb1a0e3c83ade2bdcf324f95ee38b2b31f67a02
lucasssosa <lucassiqueirasosa@gmail.com>
2024-09-14 20:50:35 -0300

    CARDS

--- app/components/card/Card.jsx
+++ app/components/card/Card.jsx
@@ -1,9 +1,28 @@
-import styles from "./card.module.css"
+import Image from "next/image";
 
 const Card = () => {
     return (
-        <div className={styles.container}>
-            <div className="{}"></div>
+        <div className="flex m-10 b rounded-lg bg-white shadow-lg shrink-0 shadow-black-500/50 ">
+            <div className="flex flex-col">
+                <Image
+                    src="/img/ARTE_-_ELEG0018.jpg"
+                    alt="Logo"
+                    className=''
+                    height={1}
+                    width={180}
+                    objectFit='cover'
+                    objectPosition='center'
+                />
+            </div>
+
+            <div className="p-5">
+                <h1>AMAR0199 - 517BL000024</h1>
+                <ul> Pedras:
+                    <li>Sextavado black 3mm - 24700 peças</li>
+                    <li>Sextavado gold 3mm - 4439 peças</li>
+                    <li>Sextavado brow 3mm - 10547 peças</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import styles from '@/app/home.module.css';
 import clsx from 'clsx';
 import Calculo from "@/app/components/calculo/Calculo";
 import Navbar from "@/app/components/navbar/Navbar";
+import Card from "@/app/components/card/Card";
+import Input from "@/app/components/input/Input";
+import Search from "@/app/components/input/InputSearch";
 
 export default function Home() {
 
@@ -12,9 +15,17 @@ export default function Home() {
             <div>
                 <Navbar></Navbar>
             </div>
-            <div className="flex row justify-center items-center">
-                <div className="basis-1/2">
-                    <h1>LADO ESQUERDO</h1>
+            <div className="flex flex-row space-x-16 justify-center items-start">
+                <div className="basis-1/2 h-screen overflow-y-scroll">
+                    <Search placeholder="Digite o nome da peça que está buscando aqui."/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
                 </div>
                 <div className="basis-1/2">
                     <Calculo></Calculo>
