2126eaaa1052cde03ddb63de6313a4b7aa1e98cb
bruno-venturini <brunofreloventurini@outlook.com>
2024-09-15 19:33:35 -0300

    Inputs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,17 @@ const config: Config = {
     colors: {
       ...colors,
       "primary": "#445869",
-      "pastelgreen": "#70A572"
+      "pastelgreen-50": "#f4f9f4",
+      "pastelgreen-100":"#e7f1e7",
+      "pastelgreen-200": "#cfe3cf",
+      "pastelgreen-300": "#a9ccab",
+      "pastelgreen-400": "#70A572",
+      "pastelgreen-500": "#598e5b",
+      "pastelgreen-600": "#457447",
+      "pastelgreen-700": "#395c3b",
+      "pastelgreen-800": "#314a32",
+      "pastelgreen-900": "#293e2a",
+      "pastelgreen-950": "#132014"
     },
     extend: {
       backgroundImage: {
