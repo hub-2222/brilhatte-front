97b70a7dc53c1650f1ad327a1201ffdea8ab1232
bruno-venturini <brunofreloventurini@outlook.com>
2025-03-09 20:35:50 -0300

    ajustando variavel ambiente api

--- app/api/api.jsx
+++ app/api/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {redirect} from "next/navigation";
 
 export const api = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: false,
     headers: {
         "Access-Control-Allow-Origin": "*",
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
 import {Inter} from "next/font/google";
+import { Metadata } from 'next';
 import "./globals.css";
 import Home from "@/app/page";
 import Navbar from "@/app/components/navbar/Navbar";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: 'Brilhatte',
+    description: 'Site oficial da Brilhatte',
+    icons: {
+        icon: '/img/logo-brilhatte.png',
+    },
+};
 
 export default function RootLayout({
   children,
