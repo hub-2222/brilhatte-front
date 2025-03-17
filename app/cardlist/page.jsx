3545cf0f10a807074825ea6c9346b00c821ee7b5
lucasssosa <lucassiqueirasosa@gmail.com>
2025-03-16 23:20:22 -0300

    botao add

--- app/cardlist/page.jsx
+++ app/cardlist/page.jsx
@@ -316,7 +316,7 @@ export default function Page(props) {
                                         />
                                     </div>
                                     <div className="flex justify-center items-center h-full">
-                                        <span className="text-lg text-gray-500">Nenhum modelo encontrado</span>
+                                        <span className="text-lg text-gray-500">Nenhuma peça encontrado</span>
                                     </div>
                                 </div>
                             )
@@ -324,7 +324,7 @@ export default function Page(props) {
                 </div>
             </section>
             <Modal
-                title="Cadastro de Modelo"
+                title="Cadastro de Peça"
                 isOpen={isModalOpen}
                 widht="60%"
                 full={true}
@@ -336,7 +336,7 @@ export default function Page(props) {
                         <Input className="drop-shadow"
                                value={nomeCad}
                                onChange={handleChangeNomeCad}
-                               label="Digite o nome do modelo"></Input>
+                               label="Digite o nome da peça"></Input>
                         <div className="flex gap-2 w-full items-stretch">
                             <div className="w-full">
                                 <Input label="Largura"
@@ -358,7 +358,15 @@ export default function Page(props) {
                             </div>
                         </div>
                         <div className="">
-                            <span className="mb-2 text-lg font-medium text-gray-700">Tipos de pedra:</span>
+                            <div className="flex flex-col gap-2 mb-2">
+
+                                <span className="mb-2 text-lg font-medium text-gray-700">Tipos de pedra:</span>
+                                <button
+                                    onClick={addPedra}
+                                    className="p-2 w-auto justify-center drop-shadow align-middle hover:bg-blue-500 active:bg-blue-600 items-center rounded bg-blue-400 text-white">
+                                    + Adicionar tipo de pedra
+                                </button>
+                            </div>
                             <div className="flex flex-col gap-2">
                                 {
                                     pedrasCad.sort((a, b) => a.key - b.key).map((item, index) => (
@@ -375,6 +383,7 @@ export default function Page(props) {
                                     ))
                                 }
                             </div>
+                            
                         </div>
                         <div className="flex md:flex-row flex-col mt-5 md:items-start items-center">
                                 <span
@@ -409,7 +418,7 @@ export default function Page(props) {
                     setIsModalDelOpen(false)
                 }
                 }>
-                <span className="text-xl text-gray-500">Você tem certeza que deseja excluir este modelo?</span>
+                <span className="text-xl text-gray-500">Você tem certeza que deseja excluir esta peça?</span>
                 <div className="flex mt-2 justify-end gap-2">
                     <button onClick={deleteRoupa(roupaDelete?.id)}
                             className="md:w-1/5 w-1/4 text-white mt-4 px-4 py-2 rounded cursor-pointer hover:bg-pastelgreen-500 active:bg-pastelgreen-600 items-center bg-pastelgreen-400">Sim
